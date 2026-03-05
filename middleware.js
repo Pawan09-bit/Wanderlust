7120371588f4d5289d95701e1908c56444fdfbb0
Pawan09-bit <pawanjoshi15102008@gmail.com>
2026-03-05 22:31:45 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,8 +2,6 @@ const Listing = require("./models/listing");
 const Review = require("./models/review");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
-const ExpressError = require("./utils/ExpressError.js");
-const Review = require("./models/review.js");
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
@@ -24,6 +22,7 @@ module.exports.saveRedirectUrl = (req, res, next) => {
 module.exports.isOwner = async (req, res, next) => {
     const { id } = req.params;
     const listing = await Listing.findById(id);
+
     if (!listing.owner.equals(res.locals.currUser._id)) {
         req.flash("error", "You are not the owner of this listing!");
         return res.redirect(`/listings/${id}`);
@@ -37,8 +36,6 @@ module.exports.validateListing = (req, res, next) => {
     if (error) {
         const errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400, errMsg);
-    } else {
-        next();
     }
     next();
 };
@@ -48,8 +45,6 @@ module.exports.validateReview = (req, res, next) => {
     if (error) {
         const errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400, errMsg);
-    } else {
-        next();
     }
     next();
 };
@@ -57,6 +52,7 @@ module.exports.validateReview = (req, res, next) => {
 module.exports.isReviewAuthor = async (req, res, next) => {
     const { id, reviewId } = req.params;
     const review = await Review.findById(reviewId);
+
     if (!review.author.equals(res.locals.currUser._id)) {
         req.flash("error", "You are not the author of this review!");
         return res.redirect(`/listings/${id}`);
